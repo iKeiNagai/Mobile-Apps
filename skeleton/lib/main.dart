e49430d3db52749b1230b3e41a85b58c3189b4a2
iKeiNagai <thomasvasquez68@gmail.com>
2024-09-18 23:08:42 -0400

    increment by

--- skeleton/lib/main.dart
+++ skeleton/lib/main.dart
@@ -25,6 +25,7 @@ class CounterWidget extends StatefulWidget {
 
 class _CounterWidgetState extends State<CounterWidget> {
 //initial couter value
+final TextEditingController _controller1=TextEditingController();
   int _counter = 0;
   @override
   Widget build(BuildContext context) {
@@ -60,25 +61,46 @@ class _CounterWidgetState extends State<CounterWidget> {
           Row(
             children: [
               OutlinedButton(
+                onPressed:(){
+                  setState(() {
+                    if(_counter > 0){
+                      _counter--;
+                    }
+                  });
+                }, 
+              child: Text('Decrement'),
+              ),
+              OutlinedButton(
+                onPressed:(){
+                  setState(() {
+                    _counter = 0;
+                  });
+                }, 
+                child: Text('Reset'),
+                ),
+            ],
+          ),
+          Container(
+            width: 150,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Increment by:',
+              ),
+            controller: _controller1,
+            ),
+          ),
+          ElevatedButton(
             onPressed:(){
               setState(() {
-                if(_counter > 0){
-                  _counter--;
+                int? _userValue = int.tryParse(_controller1.text);
+
+                if(_userValue != null && _counter < 100){
+                  _counter += _userValue;
                 }
               });
             }, 
-            child: Text('Decrement'),
-            ),
-          OutlinedButton(
-            onPressed:(){
-              setState(() {
-                _counter = 0;
-              });
-            }, 
-            child: Text('Reset'),
-            ),
-            ],
-          )
+            child: Text('enter'),
+          ),
         ],
       ),
     );
