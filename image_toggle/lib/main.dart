ced34fa5a5ae782ab0cd7a49ebfb25f500ba61bd
iKeiNagai <thomasvasquez68@gmail.com>
2024-09-21 15:40:11 -0400

    toggle image button

--- image_toggle/lib/main.dart
+++ image_toggle/lib/main.dart
@@ -11,10 +11,17 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int _counter = 0;
+  bool _toggleimage = true;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _toggle(){
+    setState(() {
+      _toggleimage = !_toggleimage;
     });
   }
 
@@ -34,8 +41,23 @@ class _MyAppState extends State<MyApp> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: _incrementCounter,
-              child: Text('Press'),
+              child: Text('Increment'),
             ),
+            SizedBox(height: 50),
+            _toggleimage
+              ? Image.network(//expression if true
+                    'https://th.bing.com/th/id/R.4363270904f8d09d6ae08ccf2e787069?rik=YcuEo1ubKnM%2fbA&riu=http%3a%2f%2f4.bp.blogspot.com%2f-H5MjSvN-_BU%2fVYiPgHbypLI%2fAAAAAAAAAP8%2fBK2tgk28WD8%2fs1600%2fANDROID.png&ehk=z89VsaezsLzNZPKRsUU2AbNaMb0FWaGG2wLz7UhcTEc%3d&risl=&pid=ImgRaw&r=0',
+                     height: 150,
+                  )
+              : Image.network( //expression if false
+                    'https://th.bing.com/th/id/OIP.TfmFiApYF1itohHnoJehZQHaIn?rs=1&pid=ImgDetMain',
+                    height: 150,
+                  ),
+            SizedBox(height:20),
+            ElevatedButton(
+              onPressed: _toggle, 
+              child: Text('Toggle Image')
+            )
           ],
         ),
       ),
