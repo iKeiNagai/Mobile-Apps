7680edc66c1e2c90f754c0f29356ef037201e492
iKeiNagai <thomasvasquez68@gmail.com>
2024-09-25 18:07:21 -0400

    Basic UI with input field and button

--- weather_info_app/lib/main.dart
+++ weather_info_app/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App',
+      title: 'Weather App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,6 +25,26 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
       ),
       body: Center(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(labelText: 'Input City Name'),
+                  ),
+                ),
+                OutlinedButton(onPressed: () {}, child: Text('Fecth Weather')),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text("City Name"),
+            SizedBox(height: 20),
+            Text("Temperature"),
+            SizedBox(height: 20),
+            Text("Weather Condition")
+          ],
+        ),
       ),
     );
   }
