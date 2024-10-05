d9883e582149d754f2a11aaf96a6ec9dd7ffa300
iKeiNagai <thomasvasquez68@gmail.com>
2024-10-05 18:38:10 -0400

    stateful

--- boilerplate.dart
+++ boilerplate.dart
@@ -29,3 +29,49 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+//stateful
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'List of Tasks',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful Widget Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: <Widget>[
+            Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
+          
+          ],
+        ),
+      ),
+    );}}
