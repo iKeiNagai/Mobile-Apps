ca59a57b57edb6f5be2a41c6f306a39b81a08042
iKeiNagai <thomasvasquez68@gmail.com>
2024-10-05 18:05:43 -0400

    Design UI with view function

---
+++ crud/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'List of Tasks',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> ltasks = [
+    "1 task",
+    "2 task"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List of Tasks'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Padding(
+                  padding: EdgeInsets.all(25),
+                  child: TextField(
+                    decoration: InputDecoration(labelText: "Enter task name"),
+                  ),
+                )),
+                FloatingActionButton(onPressed: () {}, child: Text("Add"))
+              ],
+            ),
+            Expanded(child:ListView.builder(
+              itemCount: ltasks.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Row(
+                    children: <Widget> [
+                      Text(ltasks[index]),
+                      SizedBox(width: 30),
+                      Checkbox(value: false, 
+                      onChanged: null),
+                      OutlinedButton(onPressed:(){
+
+                      }, child: Text("remove"))
+
+                    ],
+                  ),
+                );
+              }))
+          ],
+        ),
+      ),
+    );
+  }
+}
