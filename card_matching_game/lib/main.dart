2b9c0cd9d38b581e081fb5a68f4babd5ad3e637b
iKeiNagai <thomasvasquez68@gmail.com>
2024-10-11 19:02:00 -0400

    flipping card animation

--- card_matching_game/lib/main.dart
+++ card_matching_game/lib/main.dart
@@ -20,21 +20,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<bool> flipped = List.generate(8, (_) => false);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Card Matching Game'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4, crossAxisSpacing: 8,mainAxisSpacing: 8), 
-          itemCount: 8,
-          itemBuilder: (context, index){
-              return Text("card $index");
-          }),)
-    );
+        appBar: AppBar(
+          title: Text('Card Matching Game'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, crossAxisSpacing: 8, mainAxisSpacing: 8),
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      flipped[index] = !flipped[index];
+                    });
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 500),
+                    child: Center(
+                        child: flipped[index]
+                            ? Container(
+                                color: Colors.white,
+                                child: Center(child: Text("Back")),
+                              )
+                            : Container(
+                                color: Colors.purple,
+                                child: Center(child: Text("Front")),
+                              )),
+                  ),
+                );
+              }),
+        ));
   }
 }
