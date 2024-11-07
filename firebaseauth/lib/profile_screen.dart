ac337e07df4a6eae599244361e1bf3b1e97d8003
iKeiNagai <thomasvasquez68@gmail.com>
2024-11-06 20:47:22 -0500

    change password functionality

--- firebaseauth/lib/profile_screen.dart
+++ firebaseauth/lib/profile_screen.dart
@@ -7,6 +7,58 @@ class ProfileScreen extends StatelessWidget {
 
   ProfileScreen({super.key});
 
+  void _changePassword(BuildContext context) {
+    final TextEditingController _passwordController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Change Password'),
+          content: TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: 'New Password',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final newPassword = _passwordController.text;
+                if (newPassword.isNotEmpty) {
+                  try {
+                    await _auth.currentUser?.updatePassword(newPassword);
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Password changed successfully'),
+                      ),
+                    );
+                  } catch (e) {
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Failed to change password: $e'),
+                      ),
+                    );
+                  }
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +74,9 @@ class ProfileScreen extends StatelessWidget {
               style: TextStyle(fontSize: 18),
             ),
             SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _changePassword(context), 
+              child: Text("Change Password"))
           ],
         ),
       ),
